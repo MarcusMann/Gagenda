44dfc65eb7f45ddcaa185016d36c8c75b06b32ed
Marcus <mpereirassa@gmail.com>
2017-05-09 16:30:30 -0300

    Update #2

--- main.go
+++ main.go
@@ -2,32 +2,26 @@ package main
 
 import (
 	"exercicios/gagenda/config"
+	"os"
 
-	_ "github.com/paulrademacher/climenu"
+	"github.com/paulrademacher/climenu"
 )
 
 func main() {
-	// menu := climenu.NewButtonMenu("Bem-Vindo a sua lista de contatos", "O que você gostaria de fazer?")
-	// menu.AddMenuItem("Cadastrar um Contato", "cadastrar")
-	// menu.AddMenuItem("Mostrar um Contato", "unico")
-	// menu.AddMenuItem("Mostrar todos os contatos", "todos")
-	// menu.AddMenuItem("Renomeiar contato", "renomeiar")
-	// menu.AddMenuItem("Deletar contato", "deletar")
+	menu := climenu.NewButtonMenu("Bem-Vindo a sua lista de contatos", "O que você gostaria de fazer?")
+	menu.AddMenuItem("Cadastrar um Contato", "cadastrar")
+	menu.AddMenuItem("Mostrar Contatos", "contatos")
 
-	// action, scaped := menu.Run()
-	// if scaped {
-	// 	os.Exit(1)
-	// }
+	action, scaped := menu.Run()
+	if scaped {
+		os.Exit(1)
+	}
 
-	// switch action {
-	// case "cadastrar":
-	// 	files.CreateContact()
-	// case "unico":
-	// 	files.FindContacts("marcus")
-	// case "renomeiar":
-	// 	files.RenameContacts()
-	// case "deletar":
-	// 	files.DeleteContacts()
-	// }
-	config.CreateContact()
+	switch action {
+	case "cadastrar":
+		config.CriarContato()
+	case "contatos":
+		config.EncontrarContatos()
+	}
+
 }
